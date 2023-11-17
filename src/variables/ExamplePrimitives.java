b1967e09af2facaa13fdc72dda4f7a32d3623b92
diaabashtawi <diaabashtawi@gmail.com>
2023-11-17 22:12:27 +0400

    adding ExanplePrimitives Class containe all the primivtie data type in the main method

---
+++ src/variables/ExamplePrimitives.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class ExamplePrimitives {
+
+    public static void main(String[] args) {
+        byte b = 34;
+        short s = 23000;
+        int i = 45000;
+        long l = 12345678901L;
+
+        float f = 3.2F;
+        double d = 3.232323232332;
+
+        char c1 = 'a';
+        char c2 = 24;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+    }
+}
