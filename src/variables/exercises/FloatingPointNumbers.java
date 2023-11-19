9fffb80831368779199381446e5f0e39d0212c61
diaabashtawi <diaabashtawi@gmail.com>
2023-11-19 12:46:40 +0400

    adding exercises package and adding two classes FloationPointNumbers and IntegerValue

---
+++ src/variables/exercises/FloatingPointNumbers.java
@@ -0,0 +1,13 @@
+package variables.exercises;
+
+public class FloatingPointNumbers {
+
+    public static void main(String[] args) {
+
+        float x = 3.14F;
+        float y = 4.56F;
+        float sum = x + y;
+        System.out.println("the Result of x + y : " + sum);
+
+    }
+}
---
+++ src/variables/exercises/IntegerValues.java
@@ -0,0 +1,11 @@
+package variables.exercises;
+
+public class IntegerValues {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        System.out.println("Value of a : " + a);
+        System.out.println("Value of b : " + b);
+    }
+}
