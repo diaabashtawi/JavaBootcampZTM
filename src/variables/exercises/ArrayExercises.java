31b9baae97f2b3b0b1fe3ce33e38327a9217d27c
diaabashtawi <diaabashtawi@gmail.com>
2023-11-19 13:07:34 +0400

    adding Array and String ex

---
+++ src/variables/exercises/ArrayExercises.java
@@ -0,0 +1,16 @@
+package variables.exercises;
+
+import java.util.Arrays;
+
+public class ArrayExercises {
+    public static void main(String[] args) {
+        int [] numbers = {1,2,3,4,5};
+        System.out.println("Array Elements : ");
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
